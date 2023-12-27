a7a7798a4558d1b6f6e1bb6ebb640c1161eab4e4
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2023-12-27 16:26:09 +0300

    functions

--- template/buttons/button-small.php
+++ template/buttons/button-small.php
@@ -1,1 +1,5 @@
-<button class="button-small"><span>Получить консультацию</span></button>
+<?php
+function display_custom_button($button_text = 'Получить консультацию') {
+    echo '<button class="button-small"><span>' . esc_html($button_text) . '</span></button>';
+}
+?>
