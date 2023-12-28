4ae0aef762f3bd3f84699fcbca9a24361f1f1b02
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2023-12-28 13:11:02 +0300

    image

---
+++ template/pages/main/blocks/ruby.php
@@ -0,0 +1,7 @@
+<div class="ruby">
+    <div class="ruby__item">
+        <img class="ruby__item-bg" src="<?php bloginfo('template_url'); ?>/assets/img/bg/ruby-1.svg" alt="Картинка">
+        <p class="ruby__sub-title">Специалисты</p>
+        <p class="ruby__title">Alta Via</p>
+    </div>
+</div>
