5ccd174edc524e83dc749966fcd1ac1e4b4f9ba2
GRblSHA <dron-skm1234@mail.ru>
2023-12-28 11:28:39 +0300

    Начал секцию about-us

---
+++ template/pages/main/sections/about-us.php
@@ -0,0 +1,49 @@
+<section class="about-us">
+    <div class="container">
+        <div class="about-us__body">
+            <div class="about-us__left">
+                <div class="abous-us__animation">
+                    <img class="abous-us__animation-wrapper-left" src="" alt="">
+                    <img class="abous-us__animation-centr" src="" alt="">
+                    <img class="abous-us__animation-wrapper-right" src="" alt="">
+                </div>
+            </div>
+            <div class="about-us__right">
+                <div class="about-us__cards">
+                    <article class="about-us__card">
+                        <div class="about-us__card-top">
+                            <p class="about-us__text">III</p><p class="about-us__text-desc">группа</p>
+                        </div>
+                        <div class="about-us__card-bottom">
+                            <p class="about-us__desc">в рейтинге «Право-300» в области «Медицина и фармацевтика»</p>
+                        </div>
+                    </article>
+                    <article class="about-us__card">
+                        <div class="about-us__card-top">
+                            <p class="about-us__text">4</p><p class="about-us__text-desc">раза</p>
+                        </div>
+                        <div class="about-us__card-bottom">
+                            <p class="about-us__desc">признавались лидерамив сфере «Недвижимость,строите<br>льство»</p>
+                        </div>
+                    </article>
+                    <article class="about-us__card">
+                        <div class="about-us__card-top">
+                            <p class="about-us__text">10</p><p class="about-us__text-desc">место</p>
+                        </div>
+                        <div class="about-us__card-bottom">
+                            <p class="about-us__desc">в рейтинге аналитического центра «Эксперт РА»</p>
+                        </div>
+                    </article>
+                    <article class="about-us__card">
+                        <div class="about-us__card-top">
+                            <p class="about-us__text">14</p><p class="about-us__text-desc">лет</p>
+                        </div>
+                        <div class="about-us__card-bottom">
+                            <p class="about-us__desc">на рынке юридических услуг</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
