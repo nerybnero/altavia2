d6d5ca5e5f3c14f8001db620bd52a0899abec366
GRblSHA <dron-skm1234@mail.ru>
2023-12-29 16:55:44 +0300

    Сделал под вьюпорты для 768 все блоки

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,7 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 function theme_name_scripts() {
 	
-	wp_enqueue_style( 'style', get_template_directory_uri() .'/assets/css/style.min.css');
+	wp_enqueue_style( 'style', get_template_directory_uri() .'/assets/css/style.css');
     
 	wp_enqueue_script( 'newscript', get_template_directory_uri() . '/assets/js/app.js');
 
