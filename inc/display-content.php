3bfa6ba1992d1501e6f74c359ca0ded2efcd88bd
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2023-12-31 16:07:28 +0300

    Delete inc directory

--- inc/display-content.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Вывод кнопок
-get_template_part('template/buttons/button-small');
-get_template_part('template/buttons/button-normal');
-get_template_part('template/buttons/button-secondary');
-get_template_part('template/buttons/button-services');
-
-?>
